efb7c54dd953428498ef75ae8fa83d97cec21ac4
noare001 <noah.arendt001@stud.fh-dortmund.de>
2023-11-28 16:09:06 +0100

    Rechteck zeichnen FUnktion angefangen

--- EinführungInDieProgrammierung/src/ÜbungenNoah/Übung08/Aufgabe4/RechteckTest.java
+++ EinführungInDieProgrammierung/src/ÜbungenNoah/Übung08/Aufgabe4/RechteckTest.java
@@ -1,5 +1,7 @@
 package ÜbungenNoah.Übung08.Aufgabe4;
 
+import java.util.Arrays;
+
 public class RechteckTest {
 
 	public static void main(String[] args) {
@@ -19,6 +21,47 @@ public class RechteckTest {
 			System.out.print(r1.schneidet(r4) + " ");
 			System.out.print(r1.schneidet(r5) + " ");
 			System.out.println(r3.schneidet(r2));
+			
+			drawSquares(r1, r3);
+			
 	}
 
+	public static void drawSquare(Rechteck A, String[][] ans) {
+	    for (int i = (int) A.getEckeLinksOben().getY(); i < A.getEckeLinksOben().getY() + A.getSeitenlaengeY(); i++) {
+	        for (int j = (int) A.getEckeLinksOben().getX(); j < A.getEckeLinksOben().getX() + A.getSeitenlaengeX(); j++) {
+	            if (j == A.getEckeLinksOben().getX() || j == A.getEckeLinksOben().getX() + A.getSeitenlaengeX() - 1 || i == A.getEckeLinksOben().getY() || i == A.getEckeLinksOben().getY() + A.getSeitenlaengeY() - 1) {
+	                ans[i][j] = "■ ";
+	            } else {
+	                ans[i][j] = "  ";
+	            }
+	        }
+	        }
+	}
+
+	public static void drawSquares(Rechteck A, Rechteck B) {
+	    int maxX = (int) Math.max(A.getEckeLinksOben().getX() + A.getSeitenlaengeX(), B.getEckeLinksOben().getX() + B.getSeitenlaengeX()) + 1;
+	    int maxY = (int) Math.max(A.getEckeLinksOben().getY() + A.getSeitenlaengeY(), B.getEckeLinksOben().getY() + B.getSeitenlaengeY()) + 1;
+	    
+	    String[][] ans = new String[maxY][maxX];
+
+	    for (String[] row : ans) {
+	        Arrays.fill(row, " ");
+	    }
+
+	    drawSquare(A, ans);
+	    drawSquare(B, ans);
+
+	    displayCharArray(ans);
+	}
+
+	public static void displayCharArray(String stern[][]) {
+	    for (String[] row : stern) {
+	        for (String cell : row) {
+	            System.out.print(cell);
+	        }
+	        System.out.println();
+	    }
+	}
+
+	
 }
