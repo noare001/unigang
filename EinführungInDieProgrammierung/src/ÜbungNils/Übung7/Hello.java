718eb5c5801687f0e79caa8a08b09fc72dd3d5c8
noare001 <noah.arendt001@stud.fh-dortmund.de>
2023-11-28 12:50:46 +0100

    Created Nils Folders

---
+++ EinführungInDieProgrammierung/src/ÜbungNils/Übung7/Hello.java
@@ -0,0 +1,9 @@
+package ÜbungNils.Übung7;
+
+public class Hello {
+	
+  public static void main(String[] args) {
+	System.out.println("Name: Nils Nowoczin");
+	System.out.println("Matrikelnummer: 7218114");
+  }
+}
