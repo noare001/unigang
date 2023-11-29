9d7f299ae810249065d9c006b6d836776be6991f
noare001 <noah.arendt001@stud.fh-dortmund.de>
2023-11-29 09:05:57 +0100

    Improvements on DrawSquares

--- EinführungInDieProgrammierung/src/ÜbungenNoah/Übung08/Aufgabe4/RechteckTest.java
+++ EinführungInDieProgrammierung/src/ÜbungenNoah/Übung08/Aufgabe4/RechteckTest.java
@@ -5,63 +5,65 @@ import java.util.Arrays;
 public class RechteckTest {
 
 	public static void main(String[] args) {
-			Rechteck r1 = new Rechteck(0,0,7,5);
-			System.out.println(r1);
-			Rechteck r2 = new Rechteck(6,4,2,2);
-			System.out.println(r2);
-			Rechteck r3 = new Rechteck(6,-1,2,2);
-			System.out.println(r3);
-			Rechteck r4 = new Rechteck(-1,-1,2,2);
-			System.out.println(r4);
-			Rechteck r5 = new Rechteck(-1,4,2,2);
-			System.out.println(r5);
-			
-			System.out.print(r1.schneidet(r2) + " ");
-			System.out.print(r1.schneidet(r3) + " ");
-			System.out.print(r1.schneidet(r4) + " ");
-			System.out.print(r1.schneidet(r5) + " ");
-			System.out.println(r3.schneidet(r2));
-			
-			drawSquares(r1, r3);
-			
+		Rechteck r1 = new Rechteck(0, 0, 7, 5);
+		System.out.println(r1);
+		Rechteck r2 = new Rechteck(6, 4, 2, 2);
+		System.out.println(r2);
+		Rechteck r3 = new Rechteck(6, -1, 2, 2);
+		System.out.println(r3);
+		Rechteck r4 = new Rechteck(-1, -1, 2, 2);
+		System.out.println(r4);
+		Rechteck r5 = new Rechteck(-1, 4, 2, 2);
+		System.out.println(r5);
+
+		System.out.print(r1.schneidet(r2) + " ");
+		System.out.print(r1.schneidet(r3) + " ");
+		System.out.print(r1.schneidet(r4) + " ");
+		System.out.print(r1.schneidet(r5) + " ");
+		System.out.println(r3.schneidet(r2));
+
+		drawSquares(r1, r2);
+
 	}
 
 	public static void drawSquare(Rechteck A, String[][] ans) {
-	    for (int i = (int) A.getEckeLinksOben().getY(); i < A.getEckeLinksOben().getY() + A.getSeitenlaengeY(); i++) {
-	        for (int j = (int) A.getEckeLinksOben().getX(); j < A.getEckeLinksOben().getX() + A.getSeitenlaengeX(); j++) {
-	            if (j == A.getEckeLinksOben().getX() || j == A.getEckeLinksOben().getX() + A.getSeitenlaengeX() - 1 || i == A.getEckeLinksOben().getY() || i == A.getEckeLinksOben().getY() + A.getSeitenlaengeY() - 1) {
-	                ans[i][j] = "■ ";
-	            } else {
-	                ans[i][j] = "  ";
-	            }
-	        }
-	        }
+		for (int i = (int) A.getY(); i < A.getY() + A.getSeitenlaengeY(); i++) {
+			for (int j = (int) A.getX(); j < A.getX()
+					+ A.getSeitenlaengeX(); j++) {
+				if (j == A.getX() || j == A.getX() + A.getSeitenlaengeX()-1
+						|| i == A.getY()
+						|| i == A.getY() + A.getSeitenlaengeY()-1) {
+					ans[i][j] = "■ ";
+				}
+			}
+		}
 	}
 
 	public static void drawSquares(Rechteck A, Rechteck B) {
-	    int maxX = (int) Math.max(A.getEckeLinksOben().getX() + A.getSeitenlaengeX(), B.getEckeLinksOben().getX() + B.getSeitenlaengeX()) + 1;
-	    int maxY = (int) Math.max(A.getEckeLinksOben().getY() + A.getSeitenlaengeY(), B.getEckeLinksOben().getY() + B.getSeitenlaengeY()) + 1;
-	    
-	    String[][] ans = new String[maxY][maxX];
+		int maxX = (int) Math.max(A.getX() + A.getSeitenlaengeX(),
+				B.getX() + B.getSeitenlaengeX());
+		int maxY = (int) Math.max(A.getY() + A.getSeitenlaengeY(),
+				B.getY() + B.getSeitenlaengeY());
 
-	    for (String[] row : ans) {
-	        Arrays.fill(row, " ");
-	    }
+		String[][] ans = new String[maxY][maxX];
 
-	    drawSquare(A, ans);
-	    drawSquare(B, ans);
+		for (String[] row : ans) {
+			Arrays.fill(row, "  ");
+		}
 
-	    displayCharArray(ans);
+		drawSquare(A, ans);
+		drawSquare(B, ans);
+
+		displayCharArray(ans);
 	}
 
 	public static void displayCharArray(String stern[][]) {
-	    for (String[] row : stern) {
-	        for (String cell : row) {
-	            System.out.print(cell);
-	        }
-	        System.out.println();
-	    }
+		for (int i = stern.length-1; i >= 0; i--) { // int i = stern.length-1; i >= 0; i--  -> Zentrum von Koordinatensystem oben links
+			for (String cell : stern[i]) {
+				System.out.print(cell);
+			}
+			System.out.println();
+		}
 	}
 
-	
 }
