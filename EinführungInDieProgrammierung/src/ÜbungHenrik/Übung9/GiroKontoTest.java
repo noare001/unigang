4713bee7295aa3a7fe3e4f52f660358b75817992
conti_mobil <conti_mobil@172.22.210.253>
2023-12-05 15:47:08 +0100

    Mehr dings dings jao

--- EinführungInDieProgrammierung/src/ÜbungHenrik/Übung9/GiroKontoTest.java
+++ EinführungInDieProgrammierung/src/ÜbungHenrik/Übung9/GiroKontoTest.java
@@ -7,11 +7,15 @@ public class GiroKontoTest {
 		erstesKonto.einzahlen(2050);
 		erstesKonto.setDispokredit(1050);
 		System.out.println(erstesKonto);
-		erstesKonto.auszahlen(3200);
+		System.out.println(erstesKonto.auszahlen(3200));
+		System.out.println(erstesKonto.auszahlen(1500));
+		System.out.println(erstesKonto);
+		System.out.println("-----------------------------------------------");
 		Girokonto zweitesKonto = new Girokonto();
 		erstesKonto.überweisung(zweitesKonto, 1000);
 		System.out.println(zweitesKonto);
 		System.out.println(zweitesKonto.getKontostand());
+		System.out.println("--------------------------------------------------");
 		Girokonto drittesKonto = new Girokonto();
 		drittesKonto.einzahlen(500000);
 		drittesKonto.setDispokredit(300000);
