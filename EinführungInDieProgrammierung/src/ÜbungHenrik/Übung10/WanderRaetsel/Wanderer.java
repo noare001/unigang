2feec6517f3a383f820734530e94c2b0d5ac4bf1
conti_mobil <conti_mobil@172.22.211.218>
2023-12-12 12:25:20 +0100

    Raetsel Aufgabe von Furkan kopiert :3

---
+++ EinführungInDieProgrammierung/src/ÜbungHenrik/Übung10/WanderRaetsel/Wanderer.java
@@ -0,0 +1,30 @@
+package ÜbungHenrik.Übung10.WanderRaetsel;
+
+public class Wanderer {
+
+	private int zeit;
+	private String name;
+	private boolean ueberquert;
+	
+	public Wanderer(String name, int zeit) {
+		this.name = name;
+		this.zeit = zeit;
+	}
+	
+	public int getZeit() {
+		return zeit;
+	}
+	
+	public boolean hatUeberquert() {
+		return ueberquert;
+	}
+	
+	public void ueberquere() {
+		ueberquert = !ueberquert;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
