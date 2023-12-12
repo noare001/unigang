b450cfec5067584258c550725bf5a1e9d8e10600
AKDs-Sandbox <furkanyasinakdag@gmial.com>
2023-12-12 15:19:48 +0100

    Furkan. Kleines zeugs

--- EinführungInDieProgrammierung/src/ÜbungFurkan/ueb10/Abteilung.java
+++ EinführungInDieProgrammierung/src/ÜbungFurkan/ueb10/Abteilung.java
@@ -3,36 +3,38 @@ package ÜbungFurkan.ueb10;
 public class Abteilung {
 
 	private String bezeichnung;
-	private Mitarbeiter[] mitarbeiterArray; 
-	
+	private Mitarbeiter[] mitarbeiterArray;
+
 	public Abteilung(String bezeichnug) {
 		this.bezeichnung = bezeichnug;
 		mitarbeiterArray = new Mitarbeiter[0];
 	}
-	
+
 	public String getBezeichnung() {
 		return bezeichnung;
 	}
-	
+
 	public int getAnzahlMitarbeiter() {
 		return mitarbeiterArray.length;
 	}
-	
-	public void addMitarbeiter(Mitarbeiter mit){
-		Mitarbeiter[] newArray = new Mitarbeiter[mitarbeiterArray.length+1];
-		for(int i = 0; i < mitarbeiterArray.length;i++) {
-			newArray[i] = mitarbeiterArray[i];
+
+	public void addMitarbeiter(Mitarbeiter mit) {
+		if (getAnzahlMitarbeiter() < 15) {
+			Mitarbeiter[] newArray = new Mitarbeiter[mitarbeiterArray.length + 1];
+			for (int i = 0; i < mitarbeiterArray.length; i++) {
+				newArray[i] = mitarbeiterArray[i];
+			}
+			newArray[newArray.length - 1] = mit;
+			mitarbeiterArray = newArray;
 		}
-		newArray[newArray.length-1] = mit;
-		mitarbeiterArray = newArray;
 	}
-	
+
 	public Mitarbeiter getMitarbeiter(int i) {
 		return mitarbeiterArray[i];
 	}
-	
+
 	public void ausgabeMitarbeiter() {
-		for(Mitarbeiter m : mitarbeiterArray) {
+		for (Mitarbeiter m : mitarbeiterArray) {
 			System.out.println(m.getName() + " " + m.getPersonalnummer());
 		}
 	}
