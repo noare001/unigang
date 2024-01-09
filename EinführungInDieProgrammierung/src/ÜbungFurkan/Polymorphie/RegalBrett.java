fa12bd7d052687b2571a2a3b7ac206a0014f9741
AKDs-Sandbox <furkanyasinakdag@gmial.com>
2024-01-09 11:05:31 +0100

    Furkan

--- EinführungInDieProgrammierung/src/ÜbungFurkan/Polymorphie/RegalBrett.java
+++ EinführungInDieProgrammierung/src/ÜbungFurkan/Polymorphie/RegalBrett.java
@@ -1,34 +1,36 @@
 package ÜbungFurkan.Polymorphie;
 
 public class RegalBrett {
-	
+
 	private int breite;
 	private double tragkraft;
 	private Medium[] medien;
-	
+
 	public RegalBrett(int breite, double tragkraft) {
 		medien = new Medium[100];
 		this.breite = breite;
 		this.tragkraft = tragkraft;
 	}
 
-
 	public boolean fuegeHinzu(Medium medium) {
-		if(medium.getBreite() <= breite && (tragkraft - summeGewicht()) >= medium.getGewicht() ) {
-			for(int i = 0; i<medien.length;i++) {
-				if(medien[i] == null) medien[i] = medium;
-				return true;
+		if (medium.getBreite() <= breite && (tragkraft - summeGewicht()) >= medium.getGewicht()) {
+			for (int i = 0; i < medien.length; i++) {
+				if (medien[i] == null) {
+					medien[i] = medium;
+					return true;
+				}
 			}
 		}
 		return false;
-			
+
 	}
-	
+
 	public double summeGewicht() {
 		double summe = 0;
-		for(Medium m:medien)
-			if(m != null) summe += m.getGewicht();
+		for (Medium m : medien)
+			if (m != null)
+				summe += m.getGewicht();
 		return summe;
-				
+
 	}
 }
